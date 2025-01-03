540a9a8d21e26159fad8fc614afc1f1f2973fb21
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-01-03 17:17:47 +0100

    Testing sever side event communication over htmx

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"skald_frontend/templates"
 
@@ -18,9 +19,37 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/", templ.Handler(templates.MainPage()))
+	http.HandleFunc("/events", sseHandler)
 
 	fmt.Println("Starting server on", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	// Set the necessary headers for SSE
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	// Ensure the connection stays open
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	// Send events to the client in a loop
+	for i := 0; i < 10; i++ {
+		fmt.Fprintf(w, "event: newmsg\n")
+		fmt.Fprintf(w, "data: <h3>Message: %d</h3>\n\n", i)
+		flusher.Flush()             // Ensure data is sent to the client immediately
+		time.Sleep(1 * time.Second) // Simulate a delay between messages
+	}
+
+	// Close the connection after sending all events
+	// fmt.Fprintf(w, "event: newmsg\n")
+	// fmt.Fprintf(w, "data: Closing connection\n\n")
+	// flusher.Flush()
 }
